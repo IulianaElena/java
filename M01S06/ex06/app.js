ecc4f163e5d17eb05445f5814732aafe63837747
IulianaElena <55147382+IulianaElena@users.noreply.github.com>
2019-12-10 19:51:17 +0200

    commit again

---
+++ M01S06/ex06/app.js
@@ -0,0 +1,21 @@
+const paragraphLog = (...messages) => {
+  let logContainer = document.querySelector('.logs');
+  let messageParagraph = document.createElement('p');
+  let message = '';
+
+  if (!logContainer) {
+    logContainer = document.createElement('div');
+    logContainer.classList.add('logs');
+    document.body.appendChild(logContainer);
+  }
+
+  for (let i = 0; i < messages.length; i++) {
+    message += `${messages[i]} `;
+  }
+
+  messageParagraph.innerText = message.trim();
+  logContainer.appendChild(messageParagraph);
+
+};
+
+console.domLog = paragraphLog;
--- M02S01/ex01/app.js
+++ M02S01/ex01/app.js
@@ -1,4 +1,8 @@
 class Car {
+  topSpeed = 160;
+  topReverseSpeed = -50;
+  isTrunkOpen = false;
+  areLightsOn = false;
   constructor(
     make, 
     color, 
@@ -9,6 +13,36 @@ class Car {
     this.color = color;
     this.wheels = wheels; 
     this.speed = speed;
+
+  
+  }
+
+  openTrunk() {
+    this.isTrunkOpen = true;
+  }
+
+  setRelativeSpeed(deltaSpeed) {
+    let intendedSpeed = this.speed += deltaSpeed;
+    this.setSpeed(intendedSpeed);
+  }
+
+  turnLightsOn() {
+    this.areLightsOn = true;
+  }
+  turnLightsOff() {
+    this.areLightsOn = false;
+  }
+
+  flashLights() {
+    this.turnLightsOff();
+    this.turnLightsOn();
+    window.setTimeout(() => {
+      this.turnLightsOff();
+    }, 8000)
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false;
   }
 
   accelerate() {
@@ -18,6 +52,40 @@ class Car {
   decelerate() {
     this.speed--;
   }
+
+  stop() {
+    this.speed = 0;
+    console.log(this.speed);
+  }  
+
+  setSpeed(speed) {
+    if (speed > this.topSpeed) {
+      this.speed = this.topSpeed;
+      return;
+    }
+    if (speed < this.topReverseSpeed) {
+      this.speed = this.topReverseSpeed;
+      return;
+    }
+    
+    this.speed = speed;
+    //return speed;
+  }
 }
 
 var audi = new Car('Audi', 'black', 4, 50);
+let opel = new Car('Opel', 'red', 4, 110);
+let cars = [audi, opel];
+
+cars.forEach((car) => {
+  console.log(`masina era marca ${car.make} si se deplasa cu viteza ${car.speed}.`);
+  car.decelerate();
+  car.decelerate();
+  car.decelerate();
+  car.decelerate();
+  car.decelerate();
+
+  console.log(`Viteza noua este ${car.speed}.`);
+
+});
+
