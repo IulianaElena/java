96d349c95ae441557dd6c548da8098480940f710
IulianaElena <55147382+IulianaElena@users.noreply.github.com>
2019-12-17 20:00:47 +0200

    rrr

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -1,1 +1,16 @@
 
+let stage = document.querySelector('.stage');
+let message = document.querySelector('.message');
+let countElement = document.querySelector('.count');
+let count = 0;
+
+stage.addEventListener('mouseover', () => {
+  message.innerText = 'Bleaah';
+  count = count + 1;
+});
+
+stage.addEventListener('mouseout', () => {
+  message.innerText = 'pfffui';
+  countElement.innerText = `Niliili de ${count} ori`;
+});
+
