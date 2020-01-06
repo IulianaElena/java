059cc6b39c6f92cd29909c8cc7b5925cbcdd82cf
IulianaElena <55147382+IulianaElena@users.noreply.github.com>
2020-01-06 18:37:03 +0200

    tema

---
+++ tema/app.js
@@ -0,0 +1,33 @@
+let formular = document.getElementById('formular');
+let addPersonButton = document.getElementById('addPerson');
+let personArray = new Array();
+let person;
+let list =  document.getElementById('list');
+let winner = document.getElementById('winner');
+
+class Person {
+  constructor(firstname, surname) {
+    this.firstname = firstname;
+    this.surname = surname;
+  }
+}
+
+function render (person) {
+  var nameSurname = document.createElement('li');
+  nameSurname.innerText = `${person.firstname} ${person.surname}`;
+
+  var removeButton = document.createElement('button');
+  removeButton.innerText = '-';
+  removeButton.addEventListener("click", () => {
+    list.removeChild(nameSurname);
+  });
+
+  nameSurname.appendChild(removeButton);
+  list.appendChild(nameSurname);
+}
+
+addPersonButton.addEventListener('click', () => {
+    let addName = document.getElementById('firstname');
+    let addSurname = document.getElementById('surname');
+  
+});
